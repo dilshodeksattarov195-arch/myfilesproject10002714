fd908be4fc155e284531a648cee8984959550dbc
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 20:17:34 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class notifySalidateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = notifySalidateWorker()
+    print("Worker engine initialized.")
